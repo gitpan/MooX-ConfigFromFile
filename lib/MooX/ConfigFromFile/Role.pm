fae3d3b97c00b7bf4a0dff17d8cff5b8f30f0619
Jens Rehsack <rehsack@cpan.org>
2013-09-27 17:49:30 +0000

    Import of REHSACK/MooX-ConfigFromFile-0.002 from CPAN.
    gitpan-cpan-distribution: MooX-ConfigFromFile
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         REHSACK/MooX-ConfigFromFile-0.002.tar.gz
    gitpan-cpan-author:       REHSACK
    gitpan-cpan-maturity:     released

--- lib/MooX/ConfigFromFile/Role.pm
+++ lib/MooX/ConfigFromFile/Role.pm
@@ -3,7 +3,7 @@ package MooX::ConfigFromFile::Role;
 use strict;
 use warnings;
 
-our $VERSION = '0.001';
+our $VERSION = '0.002';
 
 use Moo::Role;
 
@@ -97,6 +97,27 @@ used to construct the final object.
 While it does that, it internally calls it's own _build_* methods (I<_build_config_prefix>,
 I<_build_config_files> and I<_build_loaded_config>) unless the appropriate attributes are
 already in C<$params>.
+
+=head1 ATTRIBUTES
+
+This role uses following attributes which might be suitable customized by
+overloading the appropriate builder or pass defaults in construction arguments.
+
+=head2 config_prefix
+
+This attribute defaults to L<FindBin>'s C<$Script>. It's interpreted as the
+basename of the config file name to use.
+
+=head2 config_dirs
+
+This attribute is included from L<MooX::ConfigDir|MooX::ConfigDir/config_dirs>.
+It might be unclever to override - but possible. Use with caution.
+
+=head2 config_files
+
+This attribute contains the list of existing files in I<config_dirs> matching
+I<config_prefix> . L<Config::Any-E<gt>extensions|Config::Any/extensions>.
+Search is operated by L<File::Find::Rule>.
 
 =head1 AUTHOR
 
